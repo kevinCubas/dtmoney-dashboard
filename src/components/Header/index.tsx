e15cb6c1f607bd67a3559816c64d382df8956a14
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-02-23 02:12:53 -0300

    feat: transaction modal component & state

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./style";
 
+interface IHeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({onOpenNewTransactionModal}: IHeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
+
       </Content>
     </Container>
   )
